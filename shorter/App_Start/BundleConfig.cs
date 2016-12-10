bbbe17db297d29f35480519b3fb26b6c5bbac26c
cuongtranba <bacuongtr@gmail.com>
2016-12-10 17:41:13 +0700

    add client

--- shorter/App_Start/BundleConfig.cs
+++ shorter/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace shorter
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/page").Include(
+                        "~/Scripts/page/index.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -25,6 +28,7 @@ namespace shorter
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
---
+++ shorter/Scripts/page/index.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+    $("#link-url").keypress(function (e) {
+        e.preventDefault();
+        $(this).tooltip('toggle');
+    });
+    $("#link-url").bind("paste", function (e) {
+        var seft = $(this);
+        $(this).tooltip('hide');
+        var data = e.originalEvent.clipboardData.getData('Text');
+        if (data) {
+            seft.addClass("loadinggif");
+            seft.prop("disabled", true);
+            $.get("/home/GetShortUrl", { url: data }, function (data) {
+                seft.prop("disabled", false);
+                seft.val(data);
+                seft.select();
+                seft.removeClass("loadinggif");
+            });
+        }
+    });
+})
